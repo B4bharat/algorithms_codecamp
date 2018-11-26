5fa3783aceb835b0470ff7c213c7b411a79d845c
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2018-11-26 11:33:43 +0530

    reduce method way of reversing a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,19 +7,26 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversedString = '';
-
-  for(let character of str) {
-    reversedString = character + reversedString;
-  }
-
-  return reversedString;
+  return str.split('').reduce( (reverseString, character) =>  character + reverseString, '' );
 }
+
 
 module.exports = reverse;
 
 /**** SOLUTION #1
   function reverse(str) {
     return str.split('').reverse().join('');
+  }
+*/
+
+/**** SOLUTION #2
+  function reverse(str) {
+    let reversedString = '';
+
+    for(let character of str) {
+      reversedString = character + reversedString;
+    }
+
+    return reversedString;
   }
 */
