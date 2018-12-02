dd0708ea769c1e032b529bea07c00a43208ac8dc
Bharat Poptwani <b4bharat.available@gmail.com>
2018-12-02 16:25:25 +0530

    added solution #1 to the chunk array problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+/* 
+  SOLUTION #1
+  unchunked array is sent to you
+  
+   - create a new empty 'chunked array'
+   - the elements of 'chunked array' is gonna be the chunks of arrays that we wish to push
+   - check if the last element of 'chunked array' doesn't exists or if the last element's length is equivalent to the chunk size that we wish to create
+    - if any of the two conditions happen, create a new empty array [the chunk] and add the element which is presently being iterated, to it
+    - else
+      push the present element to it.
+   - Once the 'chunked array' is filled, return it.
+*/
+function chunk(array, size) {
+  const chunked = [];
+
+  for (const element of array) {
+    const lastElement = chunked[chunked.length - 1];
+
+    if (!lastElement || lastElement.length === size) {
+      chunked.push([element]);
+    } else {
+      lastElement.push(element);
+    }
+  }
+
+  return chunked;
+}
+
+chunk([1, 2, 3, 4], 2)
 
 module.exports = chunk;
