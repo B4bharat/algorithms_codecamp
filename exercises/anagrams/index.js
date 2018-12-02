d39d086d0fc426251feb087e9ac66bb7d6880e25
Bharat Poptwani <b4bharat.available@gmail.com>
2018-12-02 18:34:22 +0530

    added solutions for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,56 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/*
+  Regular expression
+    for removing all space and special characters - /^\w/
+*/
+function createCharMap(completeString) {
+  let charMap = {};
+
+  for (const char of completeString) {
+    if(charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  return charMap;
+}
+
+function anagrams(stringA, stringB) {
+  const modStringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  const stringAChars = modStringA.split("");
+
+  const modStringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+  const stringBChars = modStringB.split("");
+  
+  const charMapA = createCharMap(stringAChars);
+  const charMapB = createCharMap(stringBChars);
+
+  // check if the length of two arrays are equal
+  const charMapAKeyLen = Object.keys(charMapA).length;
+  const charMapBKeyLen = Object.keys(charMapB).length;
+
+  const equivalentKeyLen = charMapAKeyLen === charMapBKeyLen;
+  // check if each key in the object has equivalent values
+  let equivalentKeyValues = true;
+  for (const char in charMapA) {
+    if(charMapB[char] !== charMapA[char]) {
+      equivalentKeyValues = false;
+      break;
+    }
+  }
+
+  if (equivalentKeyLen && equivalentKeyValues) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales'))
+console.log(anagrams('Hi there', 'Bye there'))
 
 module.exports = anagrams;
