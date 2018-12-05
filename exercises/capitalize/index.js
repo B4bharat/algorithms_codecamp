d13fa124d1535952559389b4f1c90aea34766684
Bharat Poptwani <b4bharat.available@gmail.com>
2018-12-05 08:20:08 +0530

    capitalized solution #1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let capitalizedWords = [];
+
+  for (const word of str.split(' ')) {
+    capitalizedWords.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return capitalizedWords.join(' ');
+}
 
 module.exports = capitalize;
