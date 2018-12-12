5338c20e8d43ee665b724c0ea3d22c2cad834347
Bharat Poptwani <b4bharat.available@gmail.com>
2018-12-12 22:11:07 +0530

    most obvious vowel counter solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  /*
+    - convert your string to array of chars
+    - loop through your array of chars and check if the current character is 'a' or 'e' or 'i' or 'o' or 'u'
+    - if so, increment the counter
+    - post looping return the counter
+  */
+ let vowelCounter = 0;
+
+ let arrayOfChars = str.split('');
+ arrayOfChars.forEach(char => {
+   if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+     vowelCounter++
+   }
+ });
+
+ console.log(vowelCounter);
+ return vowelCounter;
+}
+
+vowels('Why?')
 
 module.exports = vowels;
