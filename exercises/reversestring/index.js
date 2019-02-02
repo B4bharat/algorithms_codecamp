18c4a286951b8dde3d26b4ace3d26bed0a073fc4
Bharat Poptwani <b4bharat.available@gmail.com>
2019-02-02 20:45:13 +0530

    reversed a string without any helper function, give me a trophy

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,17 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split('').reduce( (reverseString, character) =>  character + reverseString, '' );
-}
+  let charArray = str.split('');
 
+  let reverseArray = new Array(charArray.length);
+  let reverseArrayLen = reverseArray.length;
+
+  for (let index = 0; index < charArray.length; index++) {
+    reverseArray[reverseArrayLen - index - 1] = charArray[index];
+  }
+
+  return reverseArray.join('');
+}
 
 module.exports = reverse;
 
@@ -29,4 +37,7 @@ module.exports = reverse;
 
     return reversedString;
   }
+
+/**** SOLUTION #3
+ *  return str.split('').reduce( (reverseString, character) => character + reverseString, '' );
 */
