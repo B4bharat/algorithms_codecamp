d2fc0fe67b0e638b2a54e6fe37fe6f87285173d6
Bharat Poptwani <b4bharat.available@gmail.com>
2019-02-02 20:24:13 +0530

    string reverse revised

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,22 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+/*
+  - number of loops to run through = matrix size
+  - number of arrays one needs to create = matrix size
+  - 1st array is always straight forward, just print the number of items starting from 1, uptil the matrix size
+  - 
+*/
+
+function matrix(n) {
+  // const array1 = [1, 2];
+  // const array2 = [4, 3];
+
+  // const resultArray = [];
+  // resultArray.push(array1, array2);
+  // console.log(resultArray);
+}
+
+matrix(2)
 
 module.exports = matrix;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,26 +7,10 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split('').reduce( (reverseString, character) =>  character + reverseString, '' );
+  return str.split('').reverse().join('');
 }
+
+console.log(reverse('Greetings'));
 
 
 module.exports = reverse;
-
-/**** SOLUTION #1
-  function reverse(str) {
-    return str.split('').reverse().join('');
-  }
-*/
-
-/**** SOLUTION #2
-  function reverse(str) {
-    let reversedString = '';
-
-    for(let character of str) {
-      reversedString = character + reversedString;
-    }
-
-    return reversedString;
-  }
-*/
