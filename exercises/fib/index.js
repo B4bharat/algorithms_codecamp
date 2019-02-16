e433d9f7c9ceef653f98a86555ec7c227bf3bbec
Bharat Poptwani <b4bharat.available@gmail.com>
2019-02-16 20:02:27 +0530

    recursive solution for fibonacci added

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,16 +9,24 @@
 //   fib(4) === 3
 
 function fib(n) {
-  const result = [0, 1];
-
-  for (let i = 2; i <= n; i++) {
-    const prevNum = result[i - 1];
-    const beforePrevNum = result[i - 2];
-    
-    result.push(prevNum + beforePrevNum);
+  if (n < 2) {
+    return n;
   }
 
-  return result[n];
+  return fib(n - 1) + fib(n - 2);
 }
 
 module.exports = fib;
+
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const prevNum = result[i - 1];
+//     const beforePrevNum = result[i - 2];
+    
+//     result.push(prevNum + beforePrevNum);
+//   }
+
+//   return result[n];
+// }
