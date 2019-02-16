e9c5d7f1c33931e2cb0c1ef4cd8d91109f016bbc
Bharat Poptwani <b4bharat.available@gmail.com>
2019-02-16 19:48:11 +0530

    iterative solution for fibonacci series

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,17 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const prevNum = result[i - 1];
+    const beforePrevNum = result[i - 2];
+    
+    result.push(prevNum + beforePrevNum);
+  }
+
+  return result[n];
+}
 
 module.exports = fib;
