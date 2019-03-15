aef0e6c6a3d260a46be1f380872dc10960a627a0
Bharat Poptwani <b4bharat.available@gmail.com>
2019-03-15 08:26:06 +0530

    added 'getLast' method for linkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -42,6 +42,20 @@ class LinkedList {
   getFirst() {
     return this.head;
   }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
 }
 
 
