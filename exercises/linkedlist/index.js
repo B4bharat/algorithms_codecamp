b2e99f40a3f8fd443b08260e08daa0f1128700a8
Bharat Poptwani <b4bharat.available@gmail.com>
2019-03-20 08:20:38 +0530

    added 'removeLast' method in the LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -68,6 +68,26 @@ class LinkedList {
     
     this.head = this.head.next;
   }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  }
 }
 
 
