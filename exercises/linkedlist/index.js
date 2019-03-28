7b886e9816982461afe3fc3c37973f7a57ad37ce
Bharat Poptwani <bharat.poptwani@gmail.com>
2019-03-28 16:23:45 +0530

    added for ...of loop

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -170,6 +170,14 @@ class LinkedList {
       counter++;
     }
   }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
+  }
 }
 
 
