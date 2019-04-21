ad527f205e8fda84d36a4a43416d5d5143132fe5
Bharat Poptwani <b4bharat.available@gmail.com>
2019-04-21 19:43:06 +0530

    added 'insert' method to the BST

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,28 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+  
+  insert(data) {
+    // if data is less than the present root node and there is already a node on the left, then continue recursion
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    // if data is less than the present node, be it root or the ones on the left, and there is no node to their left, then create a new node on the left and insert this data there
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    // if data is greater than the present root node and there is already a node on the right, then continue recursion
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    // if data is greater than the present node, be it root or the ones on the right, and there is no node to their right, then create a new node on the right and insert this data there
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+}
 
 module.exports = Node;
