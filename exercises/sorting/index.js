31df6a6006f334a0c54622e1fab59859bd7e144d
Bharat Poptwani <b4bharat.available@gmail.com>
2019-05-26 21:27:39 +0530

    implemented bubbleSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,8 +1,22 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+// [4, 2 , 1, 3]
+// i = 0
+// j = 0
 
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        const lesser = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+
+  return arr;
 }
 
 function selectionSort(arr) {
