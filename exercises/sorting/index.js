16fdf773d0270ea54ddd2c388c47c825e22adf11
Bharat Poptwani <b4bharat.available@gmail.com>
2019-05-27 08:12:54 +0530

    merge function can join two sorted arrays

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -43,8 +43,20 @@ function mergeSort(arr) {
 
 }
 
+// merge will always work with two already sorted arrays
+// [-39, 15]   [22, 44]
 function merge(left, right) {
+  const results = [];
 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
